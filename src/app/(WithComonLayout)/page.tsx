255d6da98dfe7a59cb0bc13335293ef92efecf14
mazhars-git <mazharulislam611@gmail.com>
2025-02-15 07:43:11 +0600

    context

--- src/app/(WithComonLayout)/page.tsx
+++ src/app/(WithComonLayout)/page.tsx
@@ -1,6 +1,9 @@
 import { Button } from "@/components/ui/button";
+import { getCurrentUser } from "@/services/AuthService";
 
-const HomePage = () => {
+const HomePage = async () => {
+  const user = await getCurrentUser();
+  console.log(user);
   return (
     <div>
       <Button>Click Here</Button>
--- src/services/AuthService/index.ts
+++ src/services/AuthService/index.ts
@@ -1,3 +1,7 @@
+"use server";
+
+import { jwtDecode } from "jwt-decode";
+import { cookies } from "next/headers";
 import { FieldValues } from "react-hook-form";
 
 export const registerUser = async (userData: FieldValues) => {
@@ -25,9 +29,27 @@ export const loginUser = async (userData: FieldValues) => {
       },
       body: JSON.stringify(userData),
     });
-    return res.json();
+
+    const result = await res.json();
+    if (result.success) {
+      (await cookies()).set("accessToken", result.data.accessToken);
+    }
+
+    return result;
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
   } catch (error: any) {
     return Error(error);
+  }
+};
+
+export const getCurrentUser = async () => {
+  const accessToken = (await cookies()).get("accessToken")!.value;
+  let decodedData = null;
+
+  if (accessToken) {
+    decodedData = await jwtDecode(accessToken);
+    return decodedData;
+  } else {
+    return null;
   }
 };
