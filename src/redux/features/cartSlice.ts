198207beaaec6f7eaa335d917b554d047ab4d529
mazhars-git <mazharulislam611@gmail.com>
2025-04-11 00:31:47 +0600

    cart section

--- src/redux/features/cartSlice.ts
+++ src/redux/features/cartSlice.ts
@@ -2,8 +2,12 @@ import { IProduct } from "@/types";
 import { createSlice } from "@reduxjs/toolkit";
 import { RootState } from "../store";
 
+export interface CartProduct extends IProduct {
+  orderQuantity: number;
+}
+
 interface InitialState {
-  products: IProduct[];
+  products: CartProduct[];
 }
 
 const initialState: InitialState = {
@@ -15,7 +19,42 @@ const cartSlice = createSlice({
   initialState,
   reducers: {
     addProduct: (state, action) => {
-      state.products.push(action.payload);
+      const productToAdd = state.products.find(
+        (product) => product._id === action.payload._id
+      );
+
+      if (productToAdd) {
+        productToAdd.orderQuantity += 1;
+        return;
+      }
+
+      state.products.push({ ...action.payload, orderQuantity: 1 });
+    },
+
+    incrementOrderQuantity: (state, action) => {
+      const productToIncrement = state.products.find(
+        (product) => product._id === action.payload
+      );
+
+      if (productToIncrement) {
+        productToIncrement.orderQuantity += 1;
+        return;
+      }
+    },
+    decrementOrderQuantity: (state, action) => {
+      const productToDecrement = state.products.find(
+        (product) => product._id === action.payload
+      );
+
+      if (productToDecrement && productToDecrement.orderQuantity > 1) {
+        productToDecrement.orderQuantity -= 1;
+        return;
+      }
+    },
+    removeProduct: (state, action) => {
+      state.products = state.products.filter(
+        (product) => product._id !== action.payload
+      );
     },
   },
 });
@@ -23,5 +62,10 @@ const cartSlice = createSlice({
 export const orderedProductsSelector = (state: RootState) => {
   return state.cart.products;
 };
-export const { addProduct } = cartSlice.actions;
+export const {
+  addProduct,
+  incrementOrderQuantity,
+  decrementOrderQuantity,
+  removeProduct,
+} = cartSlice.actions;
 export default cartSlice.reducer;
